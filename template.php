2ff5a7f0022fd642ade9b0c21279a1b9879413f6
MITRAIS\Shulha_U3218 <sulha199@gmail.com>
2018-10-24 17:00:35 +0700

    Change region structure

--- template.php
+++ template.php
@@ -1,6 +1,17 @@
 <?php
 
-/**
- * @file
- * The primary PHP file for this theme.
- */
+function geeklabel_preprocess_html(&$vars)
+{
+    global $user;
+    $viewport = array(
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' => 'viewport',
+            'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+        ),
+    );
+    drupal_add_html_head($viewport, 'viewport');
+    drupal_add_js(drupal_get_path('theme', 'geeklabel') . '/javascripts/ocSlider.js');
+}
+
+?>
